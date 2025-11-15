10fe1c97f2e2025d67405806a2db0dfc8ae0a079
Coşkun Deniz <coskun_deniz_92@hotmail.com>
2025-11-15 23:31:01 +0300

    New methods added

--- api/routes/product.js
+++ api/routes/product.js
@@ -6,21 +6,31 @@ const Product = require('../models/product');
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        listProduct: [
-            { id: 1, name: "Halı" },
-            { id: 2, name: "Sehpa" }
-        ]
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 })
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        name: (id + "_random"),
-        id: id
-    })
-})
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
 
 router.post('/', (req, res, next) => {
     const product = new Product({
@@ -28,16 +38,50 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     });
-    
+
     product.save().then(result => {
         console.log(result);
-    }).catch(err => console.log(err));
-
-    res.status(200).json({
-        message: "Product was listed",
-        createdProduct: product
-    })
+        res.status(200).json({
+            message: "Product was listed",
+            createdProduct: result
+        })
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: err });
+    });
 })
 
+router.patch("/:productId", (req, res, next) => {
+    const id = req.params.productId
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.deleteOne({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+})
 
 module.exports = router;
