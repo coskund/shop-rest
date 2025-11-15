0169385f8d2fd58992ef21d56358c1e2e99fd204
Coşkun Deniz <coskun_deniz_92@hotmail.com>
2025-11-15 03:54:23 +0300

    Mongo db added

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const routeProduct = require('./api/routes/product');
 const routeOrder = require('./api/routes/order');
+
+const MONGO_URI = 'mongodb://localhost:27017/shop-api';
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log('✅ Connected to MongoDB'))
+    .catch((err) => console.error('❌ MongoDB connection error:', err));
+
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
