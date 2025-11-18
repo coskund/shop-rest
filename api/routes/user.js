7cab537daff2e7ddf14e787e7ac4390a9fa1c721
Coşkun Deniz <coskun_deniz_92@hotmail.com>
2025-11-18 18:16:29 +0300

    JWT added to project

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -43,6 +44,53 @@ router.post('/signup', (req, res, next) => {
             }
         })
 });
+
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                console.log('process.env.JWT_KEY');
+                console.log(process.env.JWT_KEY);
+
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        token: token,
+                        message: 'Auth successful'
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth failed'
+                });
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+})
 
 router.delete('/:userId', (req, res, next) => {
     User.deleteOne({ _id: req.params.userId })
